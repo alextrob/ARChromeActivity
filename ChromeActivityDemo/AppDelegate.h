2240ba89a5c91371dece99a3dc2a02ec1bb7f41d
Alex Robinson <alextrob@gmail.com>
2012-10-19 22:57:05 +1000

    Initial Commit

---
+++ ChromeActivityDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChromeActivityDemo
+//
+//  Created by Alex Robinson on 19/10/12.
+//  Copyright (c) 2012 Alex Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChromeActivityDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChromeActivityDemo
+//
+//  Created by Alex Robinson on 19/10/12.
+//  Copyright (c) 2012 Alex Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
